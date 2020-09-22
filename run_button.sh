58206f0a886d539fce379653a698c46f7310a544
jacksj12 <30806811+jacksj12@users.noreply.github.com>
2020-09-22 07:28:27 -0700

    Update run_button.sh
    Changed to all Java files

--- run_button.sh
+++ run_button.sh
@@ -1,5 +1,2 @@
-javac src/main/java/Construction.java
-javac src/main/java/ConstructionTester.java
-javac src/main/java/HowFarAway.java
-javac src/main/java/GeoLocation.java
+javac src/main/java/*.java
 java -cp src/main/java/ ConstructionTester
